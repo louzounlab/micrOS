c720258a852cb0c5f1d7ac8115bd902b9f89c8e0
YuliTshuva <yuli.tshuva@gmail.com>
2024-03-04 09:06:27 +0200

    Add samba and mimic

--- app/app.py
+++ app/app.py
@@ -3,10 +3,15 @@ Yuli Tshuva
 """
 
 import os
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, request, render_template, flash
+import traceback
 import secrets
+from run_models import total
 import shutil
+import time
 from datetime import datetime, timedelta
+
 
 random_hex = secrets.token_hex()
 
@@ -14,8 +19,6 @@ app = Flask(__name__, static_folder='static', template_folder="templates")
 app.config['UPLOAD_FOLDER'] = os.path.abspath("upload_folder")
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=1)
 app.config["SECRET_KEY"] = random_hex
-
-os.makedirs("static/temp_files", exist_ok=True)
 
 
 def delete_old_files_and_folders():
@@ -56,6 +59,157 @@ def delete_old_files_and_folders():
 
 
 @app.route('/impute-form', methods=['POST'])
+def impute_form():
+    try:
+        current_time = time.time()
+
+        folder = os.path.join("static", "temp_files", f"{current_time}")
+        os.mkdir(folder)
+        tasks = ["miMic", "iMic", "samba", "LOCATE"]
+        for task in tasks:
+            os.mkdir(os.path.join(folder, task))
+
+        tax_level = request.form.get("tax-level-val")
+        normalization = request.form.get("normalization-val").lower()
+        reduction = request.form.get("reduction-val")
+        tax_group = request.form.get("tax-group-val")
+        tax_level_freq = request.form.get("tax-level-freq-val")
+        z_score_log = request.form.get("z-score-log-val")
+        z_score_rel = request.form.get("z-score-rel-val")
+        epsilon = float(request.form.get("epsilon"))
+        components = int(request.form.get("components"))
+
+        OTU_file = request.files['OTU-file']
+        TAG_file = request.files['TAG-file']
+        OTU_file.save(f"static/temp_files/otu{current_time}.csv")
+        TAG_file.save(f"static/temp_files/tag{current_time}.csv")
+        OTU_file = pd.read_csv(f"static/temp_files/otu{current_time}.csv")
+        TAG_file = pd.read_csv(f"static/temp_files/tag{current_time}.csv", index_col=0)
+        os.remove(f"static/temp_files/otu{current_time}.csv")
+        os.remove(f"static/temp_files/tag{current_time}.csv")
+
+        run_iMic = True if request.form.get("iMic") else False
+        run_miMic = True if request.form.get("miMic") else False
+        run_samba = True if request.form.get("samba") else False
+        run_LOCATE = True if request.form.get("LOCATE") else False
+
+        eval = request.form.get('eval-val')
+        sis = request.form.get('sis-val')
+        correct_first = True if request.form.get('correct_first-val') == "True" else False
+        mimic_pvalue = float(request.form.get('pvalue'))
+        mimic_threshold_stat = float(request.form.get('threshold_stat'))
+
+        imic_l1_loss = float(request.form.get("l1_loss"))
+        imic_weight_decay = float(request.form.get("weight_decay"))
+        imic_lr = float(request.form.get("lr"))
+        imic_batch_size = int(request.form.get("batch_size"))
+        imic_activation = request.form.get("activation-val")
+        imic_dropout = float(request.form.get("dropout"))
+        imic_kernel_size_a = int(request.form.get("kernel_size_a"))
+        imic_kernel_size_b = int(request.form.get("kernel_size_b"))
+        imic_stride = int(request.form.get("stride"))
+        imic_padding = int(request.form.get("padding"))
+        imic_padding2 = int(request.form.get("padding_2"))
+        imic_kernel_size_a_2 = int(request.form.get("kernel_size_a_2"))
+        imic_kernel_size_b_2 = int(request.form.get("kernel_size_b_2"))
+        imic_stride_2 = int(request.form.get("stride_2"))
+        imic_channels = int(request.form.get("channels"))
+        imic_channels2 = int(request.form.get("channels_2"))
+        imic_linear_dim_divider_1 = int(request.form.get("linear_dim_divider_1"))
+        imic_linear_dim_divider_2 = int(request.form.get("linear_dim_divider_2"))
+        imic_input_dim = request.form.get("input_dim")
+
+        if imic_input_dim[0] == "(" and imic_input_dim[-1] == ")":
+            imic_input_dim = imic_input_dim.split(", ")
+            if len(imic_input_dim) == 2 and imic_input_dim[0].isdigit() and imic_input_dim[1].isdigit():
+                imic_input_dim = (int(imic_input_dim[0]), int(imic_input_dim[1]))
+            else:
+                imic_input_dim = (8, 235)
+        else:
+            imic_input_dim = (8, 235)
+
+        imic_dct = {"l1_loss": imic_l1_loss,
+                    "weight_decay": imic_weight_decay,
+                    "lr": imic_lr,
+                    "batch_size": imic_batch_size,
+                    "activation": imic_activation,
+                    "dropout": imic_dropout,
+                    "kernel_size_a": imic_kernel_size_a,
+                    "kernel_size_b": imic_kernel_size_b,
+                    "stride": imic_stride,
+                    "padding": imic_padding,
+                    "padding_2": imic_padding2,
+                    "kernel_size_a_2": imic_kernel_size_a_2,
+                    "kernel_size_b_2": imic_kernel_size_b_2,
+                    "stride_2": imic_stride_2,
+                    "channels": imic_channels,
+                    "channels_2": imic_channels2,
+                    "linear_dim_divider_1": imic_linear_dim_divider_1,
+                    "linear_dim_divider_2": imic_linear_dim_divider_2,
+                    "input_dim": imic_input_dim}
+
+        if run_LOCATE:
+            metab_file = request.files['metab']
+            metab_file.save(f"static/temp_files/{current_time}/LOCATE/input.csv")
+            metab_df = pd.read_csv(f"static/temp_files/{current_time}/LOCATE/input.csv", index_col=0)
+            os.remove(f"static/temp_files/{current_time}/LOCATE/input.csv")
+        else:
+            metab_df = None
+
+        locate_weight_decay_rep = float(request.form.get("weight_decay_rep"))
+        locate_weight_decay_dis = float(request.form.get("weight_decay_dis"))
+        locate_lr_rep = float(request.form.get("lr_rep"))
+        locate_lr_dis = float(request.form.get("lr_dis"))
+        locate_rep_coef = float(request.form.get("rep_coef"))
+        locate_dis_coef = float(request.form.get("dis_coef"))
+        locate_dropout = float(request.form.get("dropout_"))
+        locate_activation_rep = request.form.get("activation_rep-val")
+        locate_activation_dis = request.form.get("activation_dis-val")
+        locate_neurons = int(request.form.get("neurons"))
+        locate_neurons2 = int(request.form.get("neurons2"))
+        locate_representation_size = int(request.form.get("representation_size"))
+        locate_test_size = float(request.form.get("test_size"))
+
+        samba_cutoff = float(request.form.get("cutoff"))
+        samba_metric = request.form.get("metric-val")
+
+        output = total(current_time, df=OTU_file, tag=TAG_file, taxonomy_level=tax_level, taxonomy_group=tax_group,
+                       epsilon=epsilon, normalization=normalization, z_scoring=z_score_log,
+                       norm_after_rel=z_score_rel, pca=(components, reduction), run_iMic=run_iMic,
+                       run_miMic=run_miMic, run_samba=run_samba, run_LOCATE=run_LOCATE, eval=eval, sis=sis,
+                       correct_first=correct_first, cutoff=samba_cutoff, metric=samba_metric, iMic_params=imic_dct,
+                       metab_df=metab_df, test_size=locate_test_size, representation_size=locate_representation_size,
+                       weight_decay_rep=locate_weight_decay_rep, weight_decay_dis=locate_weight_decay_dis,
+                       lr_rep=locate_lr_rep, lr_dis=locate_lr_dis, rep_coef=locate_rep_coef, dis_coef=locate_dis_coef,
+                       activation_rep=locate_activation_rep, activation_dis=locate_activation_dis,
+                       neurons=locate_neurons, neurons2=locate_neurons2, dropout=locate_dropout,
+                       mimic_pvalue=mimic_pvalue, mimic_threshold_stat=mimic_threshold_stat)
+
+        render_dict = {}
+        if run_iMic:
+            render_dict["imic_train"] = os.path.join(folder, "iMic", "train.csv")
+            render_dict["imic_test"] = os.path.join(folder, "iMic", "test.csv")
+            render_dict["roc_auc_train"], render_dict["roc_auc_test"] = output[0], output[1]
+        if run_miMic:
+            mimic_folder = os.path.join(folder, "miMic")
+            render_dict["mimic_paths"] = [os.path.join(mimic_folder, file) for file in os.listdir(mimic_folder)
+                                          if file[-3:] in ["png", "svg"]]
+            render_dict["mimic_corrs"] = os.path.join(mimic_folder, "df_corrs.csv")
+        if run_samba:
+            render_dict["samba_png"] = os.path.join(folder, "samba", "umap_plot.png")
+            render_dict["samba_csv"] = os.path.join(folder, "samba", "dist_matrix.csv")
+        if run_LOCATE:
+            render_dict["n_pred"] = output[2] if run_iMic else output[0]
+            render_dict["locate_path"] = os.path.join(folder, 'LOCATE', "results.csv")
+
+        return render_template("results_second_edition.html", run_iMic=run_iMic, run_miMic=run_miMic,
+                               run_samba=run_samba, run_LOCATE=run_LOCATE, render_dict=render_dict)
+
+    except Exception as e:
+        traceback.print_exc()
+        return render_template("error.html", active="", error=str(e))
+
+
 @app.route('/', methods=['GET'])
 @app.route('/Home', methods=['GET'])
 def home():
---
+++ app/run_models.py
@@ -0,0 +1,108 @@
+"""
+Yuli Tshuva
+Calling the different models.
+"""
+
+import os
+import MIPMLP
+import SAMBA
+import samba
+from sklearn import metrics
+# import LOCATE
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import subprocess
+from mimic_da import apply_mimic
+from os.path import join
+
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if process.returncode != 0:
+        return error.decode()
+    return output.decode()
+
+
+def total(current_time, df, tag, taxonomy_level, taxonomy_group, epsilon, normalization, z_scoring, norm_after_rel, pca,
+          run_iMic, run_miMic, run_samba, run_LOCATE, eval, sis, correct_first, cutoff, metric, iMic_params, metab_df,
+          test_size, representation_size, weight_decay_rep, weight_decay_dis, lr_rep, lr_dis, rep_coef, dis_coef,
+          activation_rep, activation_dis, neurons, neurons2, dropout, mimic_pvalue, mimic_threshold_stat):
+    if taxonomy_level == "Specie":
+        taxonomy_level = 7
+    elif taxonomy_level == "Genus":
+        taxonomy_level = 6
+    elif taxonomy_level == "Family":
+        taxonomy_level = 5
+    elif taxonomy_level == "Order":
+        taxonomy_level = 4
+
+    processed = MIPMLP.preprocess(df=df, tag=tag, taxonomy_level=taxonomy_level, taxnomy_group=taxonomy_group,
+                                  epsilon=epsilon, normalization=normalization,
+                                  z_scoring=z_scoring, norm_after_rel=norm_after_rel, pca=pca)
+
+    folder = f"static/temp_files/{current_time}"
+
+    samba.micro2matrix(processed, folder, save=True)
+
+    if run_miMic:
+        path = join(folder, "miMic")
+
+        taxonomy_selected, samba_output = apply_mimic(folder, tag, eval=eval, threshold_p=mimic_pvalue,
+                                                      processed=processed, apply_samba=False, save=True)
+        if taxonomy_selected:
+            apply_mimic(folder, tag, mode="plot", tax=taxonomy_selected, eval=eval, sis=sis,
+                        correct_first=correct_first, samba_output=samba_output, save=True,
+                        threshold_p=mimic_pvalue, THRESHOLD_edge=mimic_threshold_stat, path=path)
+
+    if run_samba:
+        dm = samba.build_SAMBA_distance_matrix(folder, metric, cutoff, tag)
+        dm.to_csv(os.path.join(folder, 'samba', "dist_matrix.csv"))
+
+        if tag is not None:
+            # Saves our plot in folder/umap_plot.png
+            SAMBA.plot_umap(dm, tag, os.path.join(folder, 'samba'))
+
+        # Saves into samba folder "umat_plot.png"
+
+    if run_iMic:
+        # env1_command = "conda activate oshrit_env && python -c 'from your_module import your_function; your_function()'"
+        # output1 = run_command(env1_command)
+
+        dct = MIPMLP.apply_iMic(tag, folder, params=iMic_params)
+
+        dct["y_train"] = list(dct["y_train"])
+        dct["y_test"] = list(dct["y_test"])
+
+        fpr_train, tpr_train, thresholds_train = metrics.roc_curve(dct["y_train"], dct["pred_train"])
+        roc_auc_train = metrics.auc(fpr_train, tpr_train)
+
+        fpr_test, tpr_test, thresholds_test = metrics.roc_curve(dct["y_test"], dct["pred_test"])
+        roc_auc_test = metrics.auc(fpr_test, tpr_test)
+
+        train_path = os.path.join(folder, "iMic", "train.csv")
+        test_path = os.path.join(folder, "iMic", "test.csv")
+
+        pd.DataFrame({"pred_train": dct["pred_train"], "y_train": dct["y_train"]}).to_csv(train_path)
+        pd.DataFrame({"pred_test": dct["pred_test"], "y_test": dct["y_test"]}).to_csv(test_path)
+
+    if run_LOCATE:
+        X_train, X_test, y_train, y_test = train_test_split(processed, metab_df, test_size=test_size)
+
+        model = LOCATE.LOCATE_training(X_train, y_train, X_test, y_test, representation_size, weight_decay_rep,
+                                       weight_decay_dis, lr_rep, lr_dis, rep_coef, dis_coef,
+                                       activation_rep, activation_dis, neurons, neurons2, dropout)
+        # Prediction
+        Z_train, n_pred_tr = LOCATE.LOCATE_predict(model, X_train, y_train.columns)
+        Z_val, n_pred = LOCATE.LOCATE_predict(model, X_test, y_test.columns)
+        Z = pd.concat([Z_val, Z_train])
+        save_path = os.path.join(folder, 'LOCATE', "results.csv")
+        Z.to_csv(save_path)
+
+    if run_iMic and run_LOCATE:
+        return roc_auc_train, roc_auc_test, n_pred
+    elif run_iMic:
+        return roc_auc_train, roc_auc_test
+    elif run_LOCATE:
+        return n_pred
+    return
